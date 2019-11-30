e462307b819cd0367c5a81275a106307d3cab1a1
unknown <@OuyuanYuGuodong>
2019-11-30 20:02:47 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {
+  },
+  created () {},
+  methods: {}
+}
+</script>
+
+<style scoped></style>
